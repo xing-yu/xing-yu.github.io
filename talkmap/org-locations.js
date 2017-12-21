020703e03f146afa32ac7ee213f4b782b1419971
xing-yu <34723495+xing-yu@users.noreply.github.com>
2017-12-21 09:49:30 -0500

    change coords

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -2,16 +2,16 @@ var addressPoints = [
   [
     "Sydney, Australia",
     33.8688,
-    151.2093
+    -151.2093
   ],
   [
     "Watsonville, California",
     36.9102,
-    121.7569
+    -121.7569
   ],
   [
     "Montreal, Canada",
     45.5017,
-    73.5673
+    -73.5673
   ]
 ];
